d265f37caf2a4f14e0d5a25ae88303f3d78039a2
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-16 13:12:24 +0200

    Add project files.

---
+++ ExcerciseDataType4/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 10;
+        string b = "Szkola Dotneta";
+        double c = 10.5;
+
+        //Display data
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+    }
+}
